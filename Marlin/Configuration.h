072bb16c0838579c01f09fea08f6db57dd4f53ee
Kieran David Evans <keyz182@gmail.com>
2025-01-29 17:44:18 +0000

    Slow down a little

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1204,7 +1204,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 10, 100 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 8, 80 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
